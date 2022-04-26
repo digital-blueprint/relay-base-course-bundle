966de411977c668332d4b81d44687e351d7d064f
Christoph Reiter <reiter.christoph@gmail.com>
2022-04-26 15:05:43 +0200

    Port to ExtensionTrait

--- src/DependencyInjection/DbpRelayBaseCourseExtension.php
+++ src/DependencyInjection/DbpRelayBaseCourseExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BaseCourseBundle\DependencyInjection;
 
+use Dbp\Relay\CoreBundle\Extension\ExtensionTrait;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -11,31 +12,19 @@ use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
 class DbpRelayBaseCourseExtension extends ConfigurableExtension
 {
+    use ExtensionTrait;
+
     public function loadInternal(array $mergedConfig, ContainerBuilder $container)
     {
-        $this->extendArrayParameter(
-            $container, 'api_platform.resource_class_directories', [__DIR__.'/../Entity']);
+        $this->addResourceClassDirectory($container, __DIR__.'/../Entity');
 
-        $this->extendArrayParameter(
-            $container, 'dbp_api.paths_to_hide', [
-            '/course_attendees',
-            '/course_attendees/{identifier}',
-        ]);
+        $this->addPathToHide($container, '/course_attendees');
+        $this->addPathToHide($container, '/course_attendees/{identifier}');
 
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../Resources/config')
         );
         $loader->load('services.yaml');
-    }
-
-    private function extendArrayParameter(ContainerBuilder $container, string $parameter, array $values)
-    {
-        if (!$container->hasParameter($parameter)) {
-            $container->setParameter($parameter, []);
-        }
-        $oldValues = $container->getParameter($parameter);
-        assert(is_array($oldValues));
-        $container->setParameter($parameter, array_merge($oldValues, $values));
     }
 }
