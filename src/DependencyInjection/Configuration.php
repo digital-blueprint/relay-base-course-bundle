bee4aa9b35a3d060cf3a1eb2a6159b6b4e81a489
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2023-07-10 12:14:08 +0200

    added config defintion

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BaseCourseBundle\DependencyInjection;
 
+use Dbp\Relay\BaseCourseBundle\DataProvider\CourseDataProvider;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -11,6 +13,12 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        return new TreeBuilder('dbp_relay_course');
+        $treeBuilder = new TreeBuilder('dbp_relay_base_course');
+
+        /** @var ArrayNodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
+        CourseDataProvider::appendConfigNodeDefinitions($rootNode);
+
+        return $treeBuilder;
     }
 }
