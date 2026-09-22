4391b8f2de79c0549619f628fece6a8865ca3403
Christoph Reiter <reiter.christoph@gmail.com>
2026-09-22 15:07:42 +0200

    Port to ApiTestCase

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BaseCourseBundle\Tests;
 
-use Dbp\Relay\CoreBundle\TestUtils\AbstractApiTest;
+use Dbp\Relay\CoreBundle\TestUtils\ApiTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
-class ApiTest extends AbstractApiTest
+class ApiTest extends ApiTestCase
 {
+    public function setUp(): void
+    {
+        $this->createTestClient();
+    }
+
     public function testGetCourseItemAuthenticated(): void
     {
         $this->testClient->setUpUser(userAttributes: ['MAY_READ' => true]);
